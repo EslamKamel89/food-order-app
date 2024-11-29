da6536382ef7f34cbc8224c69ed193b72fefdd48
EslamKamel89 <elsamkamelforex@gmail.com>
2024-11-29 19:59:12 +0200

    Modal component

---
+++ src/components/UI/Modal.jsx
@@ -0,0 +1,18 @@
+import { useEffect, useRef } from "react";
+import { createPortal } from "react-dom";
+
+/* eslint-disable react/react-in-jsx-scope */
+export default function Modal({ children, open, className = "" }) {
+  useEffect(() => {
+    if (open) {
+      dialogRef.current.showModal();
+    }
+  }, [open]);
+  const dialogRef = useRef();
+  return createPortal(
+    <dialog ref={dialogRef} className={`modal ${className}`}>
+      {children}
+    </dialog>,
+    document.getElementById("modal")
+  );
+}
